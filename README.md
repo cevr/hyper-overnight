406601a64eb5182aa48ee99d9b79ae699f022e39
cvr <seeve.c@gmail.com>
2020-08-29 00:25:04 -0400

    🎉

---
+++ README.md
@@ -0,0 +1,15 @@
+# hyper-overnight
+
+> Dark [Hyper](https://hyper.is) theme with bright colors - based off the [VSCode Theme Overnight](https://github.com/cevr/overnight)
+
+![](screenshot.png)
+
+## Install
+
+```
+$ hyper i hyper-overnight
+```
+
+## Tip
+
+To get the exact same look as in the screenshot, you need to use the [Pure](https://github.com/sindresorhus/pure) prompt, [Jetbrains Mono](https://www.jetbrains.com/lp/mono/) font, and the [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting) plugin to have commands highlighted.
---
+++ index.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const colors = {
+  foreground: "#D6D9E0",
+  background: "#0E1729",
+  cursor: "#ecc48d",
+  cursorAccent: "#ecc48d",
+  selection: "rgba(38, 51, 76, 0.8)",
+  border: "#070E1B",
+};
+
+const terminalColors = {
+  black: "#2E384D",
+  white: "#D6D9E0",
+  blue: "#9AC6F2",
+  cyan: "#92D8E6",
+  green: "#BBD99E",
+  magenta: "#C1A2FF",
+  red: "#f87086",
+  yellow: "#ecc48d",
+  lightBlack: "#3D485F",
+  lightWhite: "#F2F2F2",
+  lightBlue: "#ACD1F5",
+  lightCyan: "#A5E0EC",
+  lightGreen: "#C9E2AF",
+  lightMagenta: "#CCB3FF",
+  lightRed: "#FFB3B3",
+  lightYellow: "#ecc48d",
+};
+
+exports.decorateConfig = (config) =>
+  Object.assign({}, config, {
+    backgroundColor: colors.background,
+    foregroundColor: colors.foreground,
+    borderColor: colors.border,
+    cursorColor: colors.cursor,
+    cursorAccentColor: colors.cursorAccent,
+    selectionColor: colors.selection,
+    colors: terminalColors,
+    css: `
+		/* Hide title when only one tab */
+		.tabs_title {
+      display: none !important;
+		}
+		/* Add a highlight line below the active tab */
+		.tab_tab::before {
+      content: '';
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			height: 1px;
+			background-color: ${colors.cursor};
+			transform: scaleX(0);
+			will-change: transform;
+		}
+		.tab_tab.tab_active::before {
+      transform: scaleX(1);
+			transition: all 350ms cubic-bezier(0, 0, 0.2, 1);
+		}
+		/* Fade the title of inactive tabs and the content of inactive panes */
+		.tab_text,
+		.term_term {
+      opacity: 0.6;
+			will-change: opacity;
+		}
+		.tab_active .tab_text,
+		.term_active .term_term {
+      opacity: 1;
+			transition: opacity 0.12s ease-in-out;
+		}
+		/* Allow custom css / overrides */
+		${config.css}
+	`,
+  });
